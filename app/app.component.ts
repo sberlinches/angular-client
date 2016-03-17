8c893bfc577eca171b77bec414a1b4088a74a367
Sergio Berlinches <sergioberlinches@gmail.com>
2016-03-16 19:46:58 -0700

    External template

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,14 +8,7 @@ import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
     selector: 'my-app',
-    template: `
-        <h1>{{title}}</h1>
-        <nav>
-            <a [routerLink]="['Dashboard']">Dashboard</a>
-            <a [routerLink]="['Heroes']">Heroes</a>
-        </nav>
-        <router-outlet></router-outlet>
-    `,
+    templateUrl: 'app/app.component.html',
     styleUrls: ['app/app.component.css'],
     directives: [ROUTER_DIRECTIVES],
     providers: [
