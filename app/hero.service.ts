0a609dbe33aa309fe98745068eb3cc4f670240d8
Sergio Berlinches <sergioberlinches@gmail.com>
2016-03-16 19:40:02 -0700

    Routing

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -8,10 +8,9 @@ export class HeroService {
         return Promise.resolve(HEROES);
     }
 
-    // See the "Take it slow" appendix
-    getHeroesSlowly() {
-        return new Promise<Hero[]>(resolve =>
-            setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+    getHero(id: number) {
+        return Promise.resolve(HEROES).then(
+            heroes => heroes.filter(hero => hero.id === id)[0]
         );
     }
 }
