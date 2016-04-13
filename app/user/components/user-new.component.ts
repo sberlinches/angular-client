3d1466db9221a52fb99254526c819c171e4afa6c
Sergio Berlinches <sergioberlinches@gmail.com>
2016-04-12 19:21:51 -0700

    I don't need router here

--- app/user/components/user-new.component.ts
+++ app/user/components/user-new.component.ts
@@ -1,8 +1,6 @@
 import { Component } from 'angular2/core';
-import { Router } from 'angular2/router';
 
 @Component({
-    // To do
     selector: 'user-new',
     templateUrl: 'app/user/views/user-new.component.html'
 })
