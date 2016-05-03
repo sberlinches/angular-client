392cc897c987d34e85c230585763adc9a1cf3851
Sergio Berlinches <sergioberlinches@gmail.com>
2016-05-02 20:10:55 -0700

    Syntax changed

--- app/user/services/interfaces/user.interface.ts
+++ app/user/services/interfaces/user.interface.ts
@@ -1,7 +1,8 @@
 // If we only need type checking, the interface is sufficient and lighter weight.
-interface UserInterface {
+export class UserInterface {
     id:         number;
     firstName:  string;
     lastName:   string;
     email:      string;
+    birthday:   string;
 }
