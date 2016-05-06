ea14123b2138b4af4bbc3974c540ab4196a5f23b
Sergio Berlinches <sergioberlinches@gmail.com>
2016-05-05 20:48:13 -0700

    New user form + validations + styles

--- app/user/components/user-new.component.ts
+++ app/user/components/user-new.component.ts
@@ -8,4 +8,25 @@ import { UserModel } from './../services/models/user.model';
     templateUrl: 'app/user/views/user-new.component.html'
 })
 
-export class UserNewComponent {}
+export class UserNewComponent {
+
+    countries = [
+        {'value': 'canada', 'title': 'Canada'},
+        {'value': 'japan', 'title': 'Japan'},
+        {'value': 'spain', 'title': 'Spain'},
+        {'value': 'united_states', 'title': 'United States'},
+    ];
+
+    user = new UserModel(50,'Kika','Hyangmin','kikahyangmin@example.com', this.countries[3].value);
+
+    /*
+    submitted = false;
+
+    onSubmit() {
+        this.submitted = true;
+    }
+
+    // TODO: Remove this when we're done
+    get diagnostic() { return JSON.stringify(this.user); }
+    */
+}
