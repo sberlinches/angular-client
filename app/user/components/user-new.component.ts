86b7d911fc6a9e88a76bf36335f114207d490919
Sergio Berlinches <sergioberlinches@gmail.com>
2016-05-09 21:20:22 -0700

    Reset form method + validation

--- app/user/components/user-new.component.ts
+++ app/user/components/user-new.component.ts
@@ -10,6 +10,8 @@ import { UserModel } from './../services/models/user.model';
 
 export class UserNewComponent {
 
+    active = true;
+
     countries = [
         {'value': 'canada', 'title': 'Canada'},
         {'value': 'japan', 'title': 'Japan'},
@@ -17,16 +19,30 @@ export class UserNewComponent {
         {'value': 'united_states', 'title': 'United States'},
     ];
 
-    user = new UserModel(50,'Kika','Hyangmin','kikahyangmin@example.com', this.countries[3].value);
+    user = new UserModel(50, 'name', 'lastname', 'mail@example.com', this.countries[3].value);
 
     /*
-    submitted = false;
+     Angular cannot distinguish between replacing the entire user and clearing the properties programmatically. Angular
+     makes no assumptions and leaves the control in its current, dirty state.
+
+     We'll have to reset the form controls manually with a small trick. We add an active flag to the component,
+     initialized to true. When we add a new user, we toggle active false and then immediately back to true with a quick
+     setTimeout.
+
+     This is a temporary workaround while we await a proper form reset feature.
+     */
+    newUser() {
+        this.user = new UserModel(51, '', '', '', '');
+        this.active = false;
+        setTimeout(() => this.active = true, 0);
+    }
 
     onSubmit() {
-        this.submitted = true;
+        alert(JSON.stringify(this.user));
     }
 
     // TODO: Remove this when we're done
-    get diagnostic() { return JSON.stringify(this.user); }
-    */
+    get diagnostic() {
+        return JSON.stringify(this.user);
+    }
 }
