cec21665f00f3f73f5b7f31e0f1cc67d92f5c22b
sberlinches <sergioberlinches@gmail.com>
2016-07-17 21:18:25 -0700

    User model fixed

--- app/user/models/user.model.ts
+++ app/user/models/user.model.ts
@@ -1,21 +1,19 @@
 export class UserModel {
 
     constructor(
-        public id:              number,
-        public lastName:        string,
-        public fullName:        string,
-        public city_id:         number,
-        public state_id:        number,
-        public country_id:      number,
-        public language_id:     number,
-        public birthAt:         string,
-        public createdAt:       string,
-        public updatedAt:       string,
-        public deletedAt:       string,
-        // Required
-        public username?:       string,
-        public email?:          string,
-        public password?:       string,
-        public firstName?:      string
+        public username:        string,
+        public email:           string,
+        public password:        string,
+        public firstName:       string,
+        public id?:             number,
+        public lastName?:       string,
+        public city_id?:        number,
+        public state_id?:       number,
+        public country_id?:     number,
+        public language_id?:    number,
+        public birthAt?:        string,
+        public createdAt?:      string,
+        public updatedAt?:      string,
+        public deletedAt?:      string
     ) {}
 }
