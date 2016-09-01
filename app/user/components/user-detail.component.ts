d6cae46f48ae8ada4b651ad4abd3293cd3acbbc4
sberlinches <sergioberlinches@gmail.com>
2016-09-01 00:50:30 -0700

    Input decorator no required

--- app/user/components/user-detail.component.ts
+++ app/user/components/user-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 // Models
 import { UserModel } from './../models/user.model';
