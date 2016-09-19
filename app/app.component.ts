dbf892dae070e4824204b5939385197a0096dafd
sberlinches <sergioberlinches@gmail.com>
2016-09-18 21:22:22 -0700

    Navigation bar improved

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/services/auth.service';
 
 @Component({
     selector: 'app',
     templateUrl: 'app/app.component.html'
 })
 
-export class AppComponent {}
+export class AppComponent {
+
+    constructor(
+        public authService: AuthService
+    ) {}
+}
